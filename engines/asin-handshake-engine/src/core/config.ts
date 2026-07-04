cb6a41e2e18f7c38f86f2f9183e023a43d8034a0
Dennis Christie <37033258+dbottrader@users.noreply.github.com>
2026-07-04 06:02:57 -0500

    Add default handshake engine configuration

---
+++ engines/asin-handshake-engine/src/core/config.ts
@@ -0,0 +1,12 @@
+import type { EngineConfig } from "./types";
+
+export const defaultConfig: EngineConfig = {
+  hz: 432,
+  points: 800,
+  phi: 137.507764,
+  spokes: 12,
+  step: 24,
+  love: 0.92,
+  width: 1000,
+  height: 1000
+};
